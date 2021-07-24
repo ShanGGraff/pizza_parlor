58042e11fa2a8e7711548a6a5bd353f62a447438
Shane Graff <copellius@gmail.com>
2021-07-24 15:43:25 -0700

    Remove Order object and focus on Pizza object. Might add back later

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,4 @@
 //Business Logic
-function Order() {
-  this.pizzas = {};
-  this.currentID = 0;
-}
-  
-Order.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-};
-
-Order.prototype.addPizza = function(pizza) {
-  contact.id = this.assignId();
-  this.pizzas[pizza.id] = pizza;
-};
-
-Order.prototype.findPizza = function(id) {
-  if (this.pizzas[id] != undefined) {
-    return this.pizzas[id];
-  }
-  return false;
-};
-
 
 function Pizza(size, ingredients) {
   this.size = size;
